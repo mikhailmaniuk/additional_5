075ec42666e757679ee32f2a88113af44e716abe
Misha Maniuk <mikhail.maniuk@gmail.com>
2018-09-30 21:16:00 +0300

    additional_5 task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const bracketArr = bracketsConfig.toString().split(',');
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  for (let i = 0, len = bracketArr.length; i < len; i++) {
+    if (i % 2 > 0) {
+      closeBrackets.push(bracketArr[i]);
+    } else {
+      openBrackets.push(bracketArr[i]);
+    }
+  }
+
+  let value, matchingOpeningBracket;
+  const stack = [str[0]];
+
+  for (let s = 1, len = str.length; s < len; s++) {
+    value = str[s];
+
+    if (closeBrackets.indexOf(value) > -1) {
+      matchingOpeningBracket = openBrackets[closeBrackets.indexOf(value)];
+      if (stack.indexOf(matchingOpeningBracket) > -1) {
+        if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+          return false;
+        }
+      } else {
+        stack.push(matchingOpeningBracket);
+      }
+      
+    } else {
+      stack.push(value);
+    }
+  }
+  return (stack.length == 0);
 }
